77161babc7c02e1fd7c5a20aaf507427e311d792
anero <anero79@gmail.com>
2023-03-02 15:00:03 -0300

    Add support for pagination params to organization_attendances

--- lib/mobilize_america_client/client/attendances.rb
+++ lib/mobilize_america_client/client/attendances.rb
@@ -1,11 +1,19 @@
 module MobilizeAmericaClient
   class Client
     module Attendances
-      def organization_attendances(organization_id:, updated_since: nil)
+      def organization_attendances(organization_id:, updated_since: nil, page: nil, per_page: nil)
         params = {}
 
         unless updated_since.nil?
           params[:updated_since] = updated_since.to_i
+        end
+
+        unless page.nil?
+          params[:page] = page
+        end
+
+        unless per_page.nil?
+          params[:per_page] = per_page
         end
 
         get(path: "/organizations/#{esc(organization_id)}/attendances", params: params)
