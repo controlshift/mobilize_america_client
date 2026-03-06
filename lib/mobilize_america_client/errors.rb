2766d4f0b9626d2aa6c7e051395bd183be273cad
Grey Moore <grey@controlshiftlabs.com>
2026-03-06 13:02:44 -0500

    RateLimitedError includes retry_after attribute (#16)
    * when raising RateLimitedError, include the Retry-After data in a structured way
    
    * ensure we handle various non-integer cases correctly

--- lib/mobilize_america_client/errors.rb
+++ lib/mobilize_america_client/errors.rb
@@ -9,6 +9,13 @@ module MobilizeAmericaClient
   end
 
   class RateLimitedError < ClientError
+    attr_reader :retry_after
+
+    def initialize(message, retry_after: nil)
+      @retry_after = Integer(retry_after, exception: false)
+
+      super(message)
+    end
   end
 
   class ServerError < StandardError
